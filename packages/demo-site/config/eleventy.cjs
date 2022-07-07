4e970049ca9ff4976c38a0cc09d119c637de968c
Giacomo Debidda <giacomo@giacomodebidda.com>
2022-07-07 13:33:02 +0200

    feat(demo-site): add CSS and fonts, cleanup

--- packages/demo-site/config/eleventy.cjs
+++ packages/demo-site/config/eleventy.cjs
@@ -1,19 +1,26 @@
-const path = require('node:path')
+// const path = require('node:path')
 const slugify = require('slugify')
+const navigation = require('@11ty/eleventy-navigation')
 const { telegramPlugin } = require('@jackdbd/eleventy-plugin-telegram')
 const {
   textToSpeechPlugin
 } = require('@jackdbd/eleventy-plugin-text-to-speech')
+const helmet = require('eleventy-plugin-helmet')
+const shortcodes = require('../src/shortcodes')
 
-const MONOREPO_ROOT = path.join(__filename, '..', '..', '..', '..')
-const PACKAGE_ROOT = path.join(MONOREPO_ROOT, 'packages', 'demo-site')
-const OUTPUT_DIR = path.join(PACKAGE_ROOT, '_site')
+const isProduction = () => process.env.NODE_ENV === 'production'
 
 // add a dev server as soon as it is available in Eleventy 2.0
 // https://www.11ty.dev/docs/watch-serve/
 
 module.exports = function (eleventyConfig) {
   const { chat_id: chatId, token } = JSON.parse(process.env.TELEGRAM)
+
+  // 11ty shortcodes
+  // https://www.11ty.dev/docs/shortcodes/
+  Object.keys(shortcodes).forEach((name) => {
+    eleventyConfig.addShortcode(name, shortcodes[name])
+  })
 
   eleventyConfig.addFilter('slugify', function (str) {
     return slugify(str, {
@@ -24,7 +31,7 @@ module.exports = function (eleventyConfig) {
   })
 
   eleventyConfig.addFilter('log', (value) => {
-    console.log('===', value)
+    console.log('=== LOG ===', value)
   })
 
   // https://www.11ty.dev/docs/collections/
@@ -37,6 +44,9 @@ module.exports = function (eleventyConfig) {
     return items
   })
 
+  eleventyConfig.addPlugin(helmet)
+  eleventyConfig.addPlugin(navigation)
+
   eleventyConfig.addPlugin(telegramPlugin, {
     chatId,
     token,
@@ -46,14 +56,34 @@ module.exports = function (eleventyConfig) {
 
   // const ELEVENTY_ENV = process.env.ELEVENTY_ENV
 
+  const audioHost = isProduction()
+    ? 'https://undici.pages.dev'
+    : 'http://localhost:8090'
+
   eleventyConfig.addPlugin(textToSpeechPlugin, {
+    audioAssetsDir: 'assets/audio',
     audioEncoding: 'MP3',
     // audioEncoding: 'LINEAR16',
-    audioHost: `http://localhost:8090/packages/demo-site/_site`,
+    audioHost,
+    cloudStorageBucket: 'bkt-eleventy-plugin-text-to-speech-audio-files',
+    cssSelector: 'div.audio-demos',
     // regexPattern: '.*/posts/.*.html$',
     keyFilename: process.env.GOOGLE_APPLICATION_CREDENTIALS,
     voice: { languageCode: 'en-GB', name: 'en-GB-Wavenet-C' }
   })
+
+  // Static assets
+  eleventyConfig.addPassthroughCopy({
+    'src/includes/assets/css': 'assets/css',
+    'src/includes/assets/fonts': 'assets/fonts',
+    'src/includes/assets/img': 'assets/img',
+    'src/includes/assets/js': 'assets/js'
+  })
+
+  console.log(
+    '=== available 11ty collections for demo-site ===',
+    Object.keys(eleventyConfig.collections)
+  )
 
   // https://www.11ty.dev/docs/config/#configuration-options
   return {
@@ -63,13 +93,13 @@ module.exports = function (eleventyConfig) {
       includes: 'includes',
       input: 'src',
       layouts: 'layouts',
-      output: OUTPUT_DIR
+      output: '_site'
     },
     htmlTemplateEngine: 'njk',
     markdownTemplateEngine: 'njk',
-    // pathPrefix: '/',
+    pathPrefix: '/',
     // https://www.11ty.dev/docs/config/#deploy-to-a-subdirectory-with-a-path-prefix
-    pathPrefix: '/packages/demo-site/_site/',
+    // pathPrefix: '/packages/demo-site/_site/',
     templateFormats: ['html', 'md', 'njk']
   }
 }
