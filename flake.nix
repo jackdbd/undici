ad9270363b4d597d28d8765bc0354f3f964a86c7
Giacomo Debidda <giacomo@giacomodebidda.com>
2024-04-22 13:00:27 +0200

    chore: setup nix dev shell in flake.nix

---
+++ flake.nix
@@ -0,0 +1,53 @@
+{
+  description = "A Nix-flake-based Node.js development environment";
+
+  inputs = {
+    # https://github.com/NixOS/nixpkgs/branches
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
+  };
+
+  outputs = {
+    nixpkgs,
+    self,
+    ...
+  } @ inputs: let
+    overlays = [
+      (final: prev: {
+        nodejs = prev.nodejs_21;
+      })
+    ];
+    supportedSystems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
+    forEachSupportedSystem = f:
+      nixpkgs.lib.genAttrs supportedSystems (system:
+        f {
+          pkgs = import nixpkgs {inherit overlays system;};
+        });
+  in {
+    devShells = forEachSupportedSystem ({pkgs}: {
+      default = pkgs.mkShell {
+        packages = with pkgs; [nodejs];
+
+        shellHook = ''
+          echo "Nix dev shell"
+          echo "- Node.js $(node --version)"
+          echo "- npm $(npm --version)"
+
+          # secrets exposed as environment variables
+          export CLOUDFLARE_R2=$(cat /run/secrets/cloudflare_r2/personal);
+          export ELEVENLABS_API_KEY=$(cat /run/secrets/elevenlabs/api_key);
+          export GITHUB_TOKEN=$(cat /run/secrets/github-tokens/semantic_release_bot);
+          export NPM_TOKEN=$(cat /run/secrets/npm-tokens/semantic_release_bot);
+          export PLAUSIBLE=$(cat /run/secrets/plausible/test_site);
+          export TELEGRAM=$(cat /run/secrets/telegram/personal_bot);
+        '';
+
+        CLOUDFLARE_ACCOUNT_ID = "43f9884041661b778e95a26992850715";
+        DEBUG = "script:*,Eleventy:EleventyErrorHandler";
+        # DEBUG = "script:*,Eleventy:EleventyErrorHandler,11ty-plugin:*";
+        NODE_ENV = "development";
+        SKIP_TELEGRAM_MESSAGES = 1;
+        # SKIP_VALIDATION = 1;
+      };
+    });
+  };
+}
